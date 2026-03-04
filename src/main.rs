22915de401f97ffaf5110a2bd0aceb305b0f93fa
neeeb1 <18238012+neeeb1@users.noreply.github.com>
2026-03-03 22:24:26 -0800

    Add initial state structs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,54 @@
-fn main() {
-    println!("Hello, world!");
+use ratatui::{DefaultTerminal, Frame, crossterm};
+use std::io;
+
+enum AppMode {
+    Selection,
+    Entry,
+    Exit,
+}
+
+pub struct Config {
+    prompts: Vec::<JournalPrompt>
+}
+
+pub struct JournalPrompt {
+    name: String,
+    prompt: String,
+    user_input: Option<String>,
+}
+
+pub struct App {
+    mode: AppMode,
+    available_prompts: Vec::<JournalPrompt>,
+    selected_prompts: Vec::<JournalPrompt>,
+    current_prompt: Option<JournalPrompt>,
+}
+
+impl App {
+    pub fn new(conf: Config) -> App {
+        App {
+            mode: AppMode::Selection,
+            available_prompts: conf.prompts,
+            selected_prompts: Vec::<JournalPrompt>::new(),
+            current_prompt: None,
+        }
+    }
+}
+
+fn main() -> io::Result<()> {
+    ratatui::run(app)?;
+    Ok(())
+}
+
+fn app(terminal: &mut DefaultTerminal) -> std::io::Result<()> {
+    loop {
+        terminal.draw(render)?;
+        if crossterm::event::read()?.is_key_press() {
+            break Ok(());
+        }
+    }
+}
+
+fn render(frame: &mut Frame) {
+    frame.render_widget("hello world", frame.area());
 }
